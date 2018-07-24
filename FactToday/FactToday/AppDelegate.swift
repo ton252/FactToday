f514e9b86ccb019f5c258c1ffd3b58024d108a1f
Anton Polyakov <tonwork252@gmail.com>
2018-07-24 12:24:14 +0300

    Add start project

---
+++ FactToday/FactToday/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  FactToday
+//
+//  Created by Anton Polyakov on 24/07/2018.
+//  Copyright © 2018 Anton Polyakov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ FactToday/FactToday/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FactToday
+//
+//  Created by Anton Polyakov on 24/07/2018.
+//  Copyright © 2018 Anton Polyakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
