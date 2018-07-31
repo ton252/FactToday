f4b2a91b1e8a70e0b8b920f72235844a43ed17dd
Anton Polyakov <tonwork252@gmail.com>
2018-07-31 12:18:57 +0300

    Add gestures

---
+++ FactToday/FactToday/FactModel.swift
@@ -0,0 +1,22 @@
+//
+//  FactModel.swift
+//  FactToday
+//
+//  Created by Anton Polyakov on 25/07/2018.
+//  Copyright © 2018 Anton Polyakov. All rights reserved.
+//
+
+import Foundation
+
+
+final class FactModel {
+    
+    var newFact: String! {
+        return "New string, new string, new string"
+    }
+    
+    var previousFact: String! {
+        return "Prevoius string, prevoius string, prevoius string"
+    }
+    
+}
--- FactToday/FactToday/ViewController.swift
+++ FactToday/FactToday/ViewController.swift
@@ -9,11 +9,70 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    private let factModel = FactModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        configureGestures()
+    }
+    
+    private func configureUI() {
+    }
+    
+    private func configureGestures() {
+        let left = UISwipeGestureRecognizer(target: self, action: #selector(swipeLeft))
+        let right = UISwipeGestureRecognizer(target: self, action: #selector(swipeRight))
+        left.direction = .left
+        right.direction = .right
+        view.addGestureRecognizer(left)
+        view.addGestureRecognizer(right)
+    }
+    
+    @objc private func swipeLeft() {
+        textView.setText(factModel.newFact, animated: true)
+    }
+    
+    @objc private func swipeRight() {
+        textView.setText(factModel.previousFact, animated: true)
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            textView.setText(factModel.newFact, animated: true)
+        }
     }
 
+}
+
+extension UITextView {
+    
+    func setText(_ text: String, animated: Bool = false) {
+        
+        let fadeIn: () -> () = { [unowned self] in
+            self.alpha = 0
+        }
+        let fadeOut: (Bool) -> () = { [unowned self] _ in
+            self.text = text
+            UIView.animate(withDuration: 0.5) { [unowned self] in
+                self.alpha = 1
+            }
+        }
+
+        UIView.animate(withDuration: 0.5, animations: fadeIn, completion: fadeOut)
+//        UIView.animateKeyframes(withDuration: 8.0, delay: 0, options: [], animations: {
+//            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5, animations: {
+//                self.alpha = 0
+//            })
+//            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.5, animations: {
+//                self.text = text
+//                self.alpha = 1
+//            })
+//        }, completion: nil)
+
+    }
+    
 }
 
